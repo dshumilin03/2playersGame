37079ca28d6778897225cea8903d3bfb6b23cc5f
dshumilin03 <dshumilin03@yandex.ru>
2020-04-25 15:54:02 +0300

    work in progress

--- maincode.py
+++ maincode.py
@@ -1,3 +1,39 @@
-#orijrfgkjsrengdsrklmg1414124
-#tyrty
-#',derpgkermgkodrgerdkojgmndrgjokdrmngdojrtgndsrjgoindrg
+# <editor-fold desc="imports">
+import pygame
+import sys
+import math
+# </editor-fold>
+
+pygame.display.init()
+
+# <editor-fold desc="Colors">
+black = (0, 0, 0)
+red = (255, 0, 0)
+green = (0, 255, 0)
+blue = (0, 0, 255)
+white = (255, 255, 255)
+# </editor-fold>
+
+# <editor-fold desc="Screen settings">
+Width = 800
+Height = 600
+screen = pygame.display.set_mode((Width, Height))
+# </editor-fold>
+
+game_is_running = True
+
+while game_is_running is True:
+    screen.fill((255, 255, 255))
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+
+    pygame.display.update()
+
+class Player(pygame.sprite.Sprite):
+    def __init__(self):
+        pygame.sprite.Sprite.__init__(self)
+        self.width = 10
+        self.height = 10
+
