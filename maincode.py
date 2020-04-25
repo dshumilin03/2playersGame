d12463e4aa31f02af3049b0a9071a4ffc0706137
dshumilin03 <dshumilin03@yandex.ru>
2020-04-25 16:44:27 +0300

    work in progress

--- maincode.py
+++ maincode.py
@@ -3,6 +3,7 @@ import pygame
 import sys
 import math
 from os import path
+
 # </editor-fold>
 
 pygame.display.init()
@@ -19,26 +20,52 @@ white = (255, 255, 255)
 Width = 800
 Height = 600
 screen = pygame.display.set_mode((Width, Height))
+clock = pygame.time.Clock()
 # </editor-fold>
 
 image_dir = path.join(path.dirname(__file__), 'images')
 
-pygame.image.load(image_dir)
+player_image = pygame.image.load(path.join(image_dir, "player.png"))
 
 game_is_running = True
 
-while game_is_running is True:
-    screen.fill(white)
+all_sprites = pygame.sprite.Group()
 
+
+class Player(pygame.sprite.Sprite):
+    def __init__(self):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = player_image
+        self.image.set_colorkey(white)
+        self.rect = self.image.get_rect()
+        self.speed = 2
+        self.rect.x = 400
+        self.rect.y = 400
+
+    def update(self):
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_w]:
+            self.rect.y -= self.speed
+        if keys[pygame.K_a]:
+            self.rect.x -= self.speed
+        if keys[pygame.K_s]:
+            self.rect.y += self.speed
+        if keys[pygame.K_d]:
+            self.rect.x += self.speed
+
+
+player = Player()
+
+all_sprites.add(player)
+
+while game_is_running is True:
+    clock.tick(60)
+    screen.fill(white)
+    keys = pygame.key.get_pressed()
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
 
+    all_sprites.update()
+    all_sprites.draw(screen)
     pygame.display.update()
-
-# It's better to extract this class into separate file
-class Player(pygame.sprite.Sprite):
-    def __init__(self):
-        pygame.sprite.Sprite.__init__(self)
-        self.width = 10
-        self.height = 10
