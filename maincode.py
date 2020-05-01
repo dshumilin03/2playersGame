b72c5e5d7d2b84809c4b1ab61a3be1c4add36e89
dshumilin03 <Dima200327>
2020-05-01 03:51:24 +0300

    work in progress

--- maincode.py
+++ maincode.py
@@ -38,24 +38,48 @@ class Player(pygame.sprite.Sprite):
         self.image = player_image
         self.image.set_colorkey(white)
         self.rect = self.image.get_rect()
-        self.speed = 2
-        self.rect.x = 400
-        self.rect.y = 400
+        self.speed = 3
+        self.rect.x = Width // 2 - self.rect.width // 2
+        self.rect.y = Height - 2 * self.rect.height
+
+    def shoot(self):
+        pass
 
     def update(self):
         keys = pygame.key.get_pressed()
-        if keys[pygame.K_w]:
-            self.rect.y -= self.speed
-        if keys[pygame.K_a]:
+        if keys[pygame.K_LEFT]:
             self.rect.x -= self.speed
-        if keys[pygame.K_s]:
-            self.rect.y += self.speed
-        if keys[pygame.K_d]:
+        if keys[pygame.K_RIGHT]:
             self.rect.x += self.speed
 
 
-player = Player()
+class SecondPlayer(pygame.sprite.Sprite):
+    def __init__(self):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = player_image
+        self.image.set_colorkey(white)
+        self.rect = self.image.get_rect()
+        self.speed = 3
+        self.rect.x = Width // 2 - self.rect.width // 2
+        self.rect.y = self.rect.height
 
+    def update(self):
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_KP4]:
+            self.rect.x -= self.speed
+        if keys[pygame.K_KP6]:
+            self.rect.x += self.speed
+
+class Bullet(pygame.sprite.Sprite):
+    def __init__(self):
+        pygame.sprite.Sprite.__init__(self)
+        pass
+
+
+player = Player()
+player2 = SecondPlayer()
+
+all_sprites.add(player2)
 all_sprites.add(player)
 
 while game_is_running is True:
